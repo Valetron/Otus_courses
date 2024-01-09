3f07285f9e9c412b3fefd9f96254c93ba95baac2
Valetron <notspammeplz@gmail.com>
2024-01-10 01:16:12 +0300

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+#include "lib.hpp"
+
+//int main(int argc, char** argv)
+int main()
 {
-	
+	std::cout << "Version: " << version() << "\n";
+    std::cout << "Hello, world!" << "\n";
 
 	return 0;
 }
