f3ab4ee2886f1b6132e411e5bcb5c106d2ce5173
Valetron <notspammeplz@gmail.com>
2024-03-19 01:06:40 +0300

    begin task 7

---
+++ task_7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (1 >= argc)
+    {
+        std::cerr << "No params set\n";
+        return EXIT_FAILURE;
+    }
+
+    int N = dynamic_cast<int>(argv[1]);
+
+    if ()
+
+    return EXIT_SUCCESS;
+}
