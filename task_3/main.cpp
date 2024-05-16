ce504abae6524e8fb84298b6808951517b8e10f0
Valetron <notspammeplz@gmail.com>
2024-05-16 21:34:19 +0300

    factorial functions

--- task_3/main.cpp
+++ task_3/main.cpp
@@ -1,9 +1,30 @@
 #include <map>
+#include <iostream>
+
+constexpr int factorial(int num)
+{
+    return num > 1 ? num * factorial(num - 1) : 1;
+}
+
+void fillData(std::map<int, int>& obj)
+{
+    for (std::size_t i = 0; i < 10; ++i)
+    {
+        obj[i] = factorial(i);
+    }
+}
 
 int main(void)
 {
     std::map<int, int> map_1;
     std::map<int, int> map_2;
+
+    fillData(map_1);
+
+    for (const auto& [key, value] : map_1)
+    {
+        std::cout << key << ", " << value << "\n";
+    }
 
     // my_continer_1<int>
     // my_continer_2<int>
